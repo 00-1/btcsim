b131aa271331cb5751e732a07237e35ed864aaf1
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-09 15:16:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @param {!Object} req HTTP request context.
  * @param {!Object} res HTTP response context.
  */
-exports.helloWorld = (req, res) => {
+exports.btcsim = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World!';
   res.status(200).send(message);
 };
