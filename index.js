6cb79a5c0b6a859fd9371ce2ccdf42914b581a6f
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-09 16:21:18 +0100

    post to slack

--- index.js
+++ index.js
@@ -6,5 +6,18 @@
  */
 exports.btcsim = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World!';
+
+  var request = require('request');
+
+  request.post(
+    `https://hooks.slack.com/services/${process.env.SLACK_KEY}`,
+    { json: { text: 'Hello World' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+  );
+
   res.status(200).send(message);
 };
