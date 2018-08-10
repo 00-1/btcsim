c5dfd8412e88a30b5de3debf6dcfa0d968022749
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-10 10:45:23 +0100

    ...

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ exports.btcsim = (req, res) => {
         `https://hooks.slack.com/services/${process.env.SLACK_KEY}`,
         { json: { text: 'Alright, message received.' } },
         function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log(body)
-          }
+	  console.log('Sent', error, response, body)
+            res.status(200).send('Thanks for the POST');
+	  }
         }
       );
     } else {
@@ -32,8 +32,6 @@ exports.btcsim = (req, res) => {
   } else {
 
   let message = req.query.message || req.body.message || 'Hello World!';
-
-
   
   res.status(200).send(message);
 
