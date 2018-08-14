68f86dfe0fb884ee5d88c81562c894fa0eb516a2
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-14 14:15:52 +0100

    revert

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const functions = require('firebase-functions');
 // takes a slack message and writes it to the db
 exports.btcsim = (req, res) => {
 
-  // give slack a 200 ASAP to avoid 3000ms timeout
-  // note this has to be disabled to send a meaningful response, like the challenge reply
-  console.log('responding asap')
-  res.sendStatus(200);
-
-  console.log(req.body)
-
   // only deal with POSTs
   if (req.method == 'POST') {
 
     // when first connected to bot need to respond to challenge
-    if (req.body.hasOwnProperty('challenge')) {
+    if (req.body.hasOwnProperty('challenge')) { 
       res.status(200).send({challenge: req.body.challenge});
 
     // otherwise check if the bot was mentioned
@@ -34,14 +27,13 @@ exports.btcsim = (req, res) => {
          // check if the document was already written
          if (!existing.exists) {
 
-            console.log("It exists", existing)
-
             // respond to query
             request.post(
               `https://hooks.slack.com/services/${process.env.SLACK_KEY}`,
               { json: { text: '🎑 Alright, message received. This incident will be reported.' } },
               function (error, response, body) {
-	        console.log('Sent', error, response, body)
+	        console.log('Sent slack message', error, response, body)
+                res.sendStatus(200);
               }
             );
 
@@ -49,14 +41,18 @@ exports.btcsim = (req, res) => {
             doc.set(req.body); 
 
          } else {
-           console.log("Already exists", existing.data())
+           console.log('Already exists', existing.data())
+           res.sendStatus(200);
          }
 
        })      
       .catch((err) => {
         console.log('Error getting documents', err);
+        res.sendStatus(200);
       });
     }
+  } else { 
+    res.sendStatus(200); 
   }
 };
 
