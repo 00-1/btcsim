3bb94145b39f5a298f4ae42b882fcf4706d93d38
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-14 11:38:13 +0100

    try responding with await

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ admin.initializeApp();
 exports.btcsim = functions.https.onRequest(async (req, res) => {
 
 
+  // give slack a 200 ASAP to avoid 3000ms timeout
+  // note this has to be disabled to send a meaningful response, like the challenge reply
+  console.log('responding asap')
+  const reply = await res.sendStatus(200);
+
   // log values
   console.log('method', req.method)
   console.log('body', req.body)
@@ -31,12 +36,7 @@ exports.btcsim = functions.https.onRequest(async (req, res) => {
       const existing = await doc.get()
       console.log(existing)
 
-  // give slack a 200 ASAP to avoid 3000ms timeout
-  // note this has to be disabled to send a meaningful response, like the challenge reply
-  console.log('responding asap')
-  res.sendStatus(200);
-
-      // check if the document was already written
+        // check if the document was already written
       if (!existing.exists) {
             // respond to query
             request.post(
