6d1e7e70ccdb069c591d05776ad5664a6ec805c5
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-14 12:01:04 +0100

    try 200 earl

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const request = require('request');
-//const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();    
 
@@ -12,10 +11,10 @@ admin.initializeApp();
 
 exports.btcsim = async (req, res) => {
 
-  // log values
-  console.log('method', req.method)
-  console.log('body', req.body)
-  console.log('slack url', `https://hooks.slack.com/services/${process.env.SLACK_KEY}`)
+  // give slack a 200 ASAP to avoid 3000ms timeout
+  // note this has to be disabled to send a meaningful response, like the challenge reply
+  console.log('responding asap')
+  res.sendStatus(200);
 
   // deal with posts
   if (req.method == 'POST') {
@@ -28,19 +27,15 @@ exports.btcsim = async (req, res) => {
       // check if we've already got this message
       const doc = admin.firestore().collection('messages').doc(req.body.event_id) 
       const existing = await doc.get()
-      console.log(existing)
 
-      // give slack a 200 ASAP to avoid 3000ms timeout
-      // note this has to be disabled to send a meaningful response, like the challenge reply
-      console.log('responding asap')
-      res.sendStatus(200);
+
 
       // check if the document was already written
       if (!existing.exists) {
             // respond to query
             request.post(
               `https://hooks.slack.com/services/${process.env.SLACK_KEY}`,
-              { json: { text: 'Alright, message received. It will be logged.' } },
+              { json: { text: 'Alright, message received. This incedent will be reported.' } },
               function (error, response, body) {
 	        console.log('Sent', error, response, body)
               }
@@ -48,8 +43,7 @@ exports.btcsim = async (req, res) => {
 
             // write the message to db         
             const result = await doc.set(req.body); 
-            console.log('Written document:', result)
-                 
+
       } else {
         console.log("Already exists", existing.data())
       }
