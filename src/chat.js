2d44661ef09431a20a7e020583df827147ac4666
00-1 <41901517+00-1@users.noreply.github.com>
2018-08-17 12:29:51 +0100

    remove extra console.log

--- src/chat.js
+++ src/chat.js
@@ -5,7 +5,7 @@ function chat(req, text) {
   request.post(
     `https://hooks.slack.com/services/${process.env.SLACK_KEY}`,
     { json: { text: `<@${req.body.event.user}> ${text}` } },
-    (error, response, body) => { console.log('Sent slack message', error, response, body); },
+    (error, response, body) => { /* nothing to do */ },
   );
 }
 
